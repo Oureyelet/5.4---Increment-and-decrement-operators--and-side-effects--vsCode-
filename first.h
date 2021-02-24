402021f08b962d0f89e335c0e27119a65bfe922a
Mateusz Kacperski <oureyelet@gmail.com>
2021-02-24 19:51:41 +0000

    I spend more or less hour today :)

--- first.h
+++ first.h
@@ -3,5 +3,6 @@
 #ifndef FIRST_ONE
 #define FIRST_ONE
 
+int add(int,int);
 
 #endif // end FIRST_ONE.
--- main.cpp
+++ main.cpp
@@ -14,5 +14,32 @@ int main()
 
     cout << x << ' ' << y << '\n' << endl;
 
+    /*
+    Here is another example showing the difference between the prefix and postfix versions:
+    */
+
+    int z { 5 };
+    int v { 5 };
+    cout << z << ' ' << v << '\n';
+    cout << ++z << ' ' << --v << '\n'; // prefix
+    cout << z << ' ' << v << '\n';
+    cout << z++ << ' ' << v-- << '\n'; // postfix
+    cout << z << ' ' << v << '\n' << endl;
+
+
+    /*
+    Side effects
+    */
+    int x1 = 5;
+     ++x1;
+    cout << x1 << '\n' << endl;
+    //The assignment operator in the above example has the side effect of changing the value of x permanently.
+
+    //side effects can also lead to unexpected results:
+    int x2{ 5 };
+    int value = add(x2, ++x2); // is this 5 + 6, or 6 + 6?
+    // It depends on what order your compiler evaluates the function arguments in
+    cout << value << '\n' << endl; // value could be 11 or 12, depending on how the above line evaluates!
+
     return 0;
 }
